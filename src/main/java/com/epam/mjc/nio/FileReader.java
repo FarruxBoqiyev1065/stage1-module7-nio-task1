0a77646afdf6d003072217fc853550804be0ba51
macservis <fboqiyev1065@gmail.com>
2023-04-17 10:40:19 +0500

    stage1-module7-nio-task1 updated

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,31 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try(FileInputStream fileInputStream = new FileInputStream(file)){
+            StringBuilder stringBuilder = new StringBuilder();
+            int c;
+            while ((c = fileInputStream.read()) != -1){
+                stringBuilder.append((char)c);
+            }
+            String[] split = stringBuilder.toString().split("\n");
+            profile.setName(split[0].split(": ")[1]);
+            profile.setAge(Integer.valueOf(split[1].split(": ")[1]));
+            profile.setEmail(split[2].split(": ")[1]);
+            profile.setPhone(Long.valueOf(split[3].split(": ")[1]));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
